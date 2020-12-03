5553787dcbd1dfbf55b989ff45ff8a16bd798944
srikanthgopu-321 <gopusrikanthreddy71@gmail.com>
2020-12-03 19:40:55 +0530

    added:Refactor Fuction to get working hours

--- Employ-Wage.js
+++ Employ-Wage.js
@@ -40,6 +40,37 @@
     
  }
 
+ {
+    const IS_PART_TIME = 2;
+    const IS_FULL_TIME = 1;
+    const PART_TIME_HOURS = 4;
+    const FULL_TIME_HOURS = 8;
+    const WAGE_PER_HOUR = 20;
+    
+    function getWorkingHours(empCheck){ 
+        switch (empCheck) {
+            case IS_FULL_TIME:
+            console.log("Employee is Present");  
+            return FULL_TIME_HOURS;
+    
+            case IS_PART_TIME:
+            console.log("Employee is Present");   
+            return PART_TIME_HOURS;
+            
+            default:
+            console.log("Employee is Absent");
+            return 0;
+         }
+        }
+          let empHrs = 0;
+          empCheck = Math.floor(Math.random() * 10) % 3;
+    
+          empHrs = getWorkingHours(empCheck);
+          let empWage = empHrs * WAGE_PER_HOUR;
+          console.log("emp Wage: " + empWage);
+     
+ }
+
 
 
 
