a15e65ff2e7079cde643c93fc7b3083e0bee8ae9
srikanthgopu-321 <gopusrikanthreddy71@gmail.com>
2020-12-03 19:20:36 +0530

    added:Employee is present or not

---
+++ Employ-Wage.js
@@ -0,0 +1,16 @@
+//Uc-1 Employe-Attendance
+ {
+    const IS_ABSENT = 0;
+    let empCheck = Math.floor(Math.random() * 10) % 2;
+    if(empCheck == IS_ABSENT){
+        console.log("UC-1 Employee is Absent");
+    } else{
+        console.log("UC-1 Employee is Present");
+    }
+
+ }
+
+
+
+
+
