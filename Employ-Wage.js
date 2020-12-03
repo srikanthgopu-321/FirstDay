70cf79ec7499a768e863f6ebddcc3e8826b44617
srikanthgopu-321 <gopusrikanthreddy71@gmail.com>
2020-12-03 19:53:45 +0530

    added:calculated Employee Wage for month

--- Employ-Wage.js
+++ Employ-Wage.js
@@ -41,12 +41,13 @@
  }
 
  {
+     //uc-3 refactor the code to write a function to get working hours
     const IS_PART_TIME = 2;
     const IS_FULL_TIME = 1;
     const PART_TIME_HOURS = 4;
     const FULL_TIME_HOURS = 8;
     const WAGE_PER_HOUR = 20;
-    
+
     function getWorkingHours(empCheck){ 
         switch (empCheck) {
             case IS_FULL_TIME:
@@ -69,6 +70,41 @@
           let empWage = empHrs * WAGE_PER_HOUR;
           console.log("emp Wage: " + empWage);
      
+ }
+
+ //Calculating Wages for month
+ {
+    const IS_PART_TIME = 2;
+    const IS_FULL_TIME = 1;
+    const PART_TIME_HOURS = 4;
+    const FULL_TIME_HOURS = 8;
+    const WAGE_PER_HOUR = 20;
+    const NUM_OF_WORKING_DAYS = 20;
+    
+    function getWorkingHours(empCheck){ 
+        switch (empCheck) {
+            case IS_FULL_TIME:
+            console.log("Employee is Present");  
+            return FULL_TIME_HOURS;
+    
+            case IS_PART_TIME:
+            console.log("Employee is Present");   
+            return PART_TIME_HOURS;
+            
+            default:
+            console.log("Employee is Abscent");
+            return 0;
+         }
+        }
+          let empHrs = 0;
+          empCheck = Math.floor(Math.random() * 10) % 3;
+    
+          for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+            let empCheck = Math.floor(Math.random() * 10) % 3;
+               empHrs += getWorkingHours(empCheck);
+            }
+             let empWage = empHrs * WAGE_PER_HOUR;
+             console.log("Total Hrs: " +empHrs+ " emp Wage: " + empWage);
  }
 
 
